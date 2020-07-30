49e259d167e5d86ff1ce69dc49bb58d59d592819
arul-aw <sukmah.bahrul@gmail.com>
2020-07-30 22:09:31 +0800

    Menghilangkan menu tidak berfungsi di admin new.qnc

--- new/admin/index.php
+++ new/admin/index.php
@@ -94,7 +94,7 @@ if($_SESSION['level']!='admin2'){
         </li>
         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-book"></i><span class="app-menu__label">Data</span><i class="treeview-indicator fa fa-angle-right"></i></a>
           <ul class="treeview-menu"> 
-            <li><a class="treeview-item" href="#"><i class="icon fa fa-circle"></i> Cucian Express</a></li>
+            <!-- <li><a class="treeview-item" href="#"><i class="icon fa fa-circle"></i> Cucian Express</a></li> -->
             <li><a class="treeview-item" href="?r=tabel&v=data-customer"><i class="icon fa fa-circle"></i> Customer</a></li>     
             <li><a class="treeview-item" href="?r=tabel&v=ontime-performance"><i class="icon fa fa-circle"></i> Terlambat/OTP</a></li>
           </ul>
