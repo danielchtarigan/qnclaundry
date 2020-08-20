f57a57d8a97446e7d9c74b1799c5257400d76963
arul <sukmah.bahrul@gmail.com>
2020-08-20 13:37:55 +0800

    perbaiki salah ketik

--- apps/controllers/MembershipController.php
+++ apps/controllers/MembershipController.php
@@ -4,7 +4,7 @@ class MembershipController extends Controller {
 
     public function index($outlet)
     {
-        if (Authorize::accessGranted($_POST['tokne'])){
+        if (Authorize::accessGranted($_POST['token'])){
             if (empty($_POST)) {
                 $_POST['start_at'] = "2020-01-01";
                 $_POST['end_at'] = date('Y-m-d');
